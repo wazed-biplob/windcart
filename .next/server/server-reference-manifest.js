a378f1069c78559bee614f01d2bbce2bcefd4871
wazed-biplob <www.biplob@gmail.com>
2025-06-13 22:45:54 +0600

    .next removed

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4rvjh6cTVh7ngZX2OApKYaDSFEfyfXUR+rWN7xa96zg=\"\n}"
--- .next/static/chunks/app_register_page_tsx_2e2394af._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/f07ee_next_01df0134._.js",
-  "static/chunks/app_register_page_tsx_e7797bdb._.js"
-],
-    source: "dynamic"
-});
